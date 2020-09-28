1d251b142acac556c24e4d192352ac317c208f89
Jithendra <Jithendra@DESKTOP-OV7PN8J>
2020-09-28 18:34:49 +0530

    Changes in DateAndYearSubtract

--- myProgram/src/testPrograms/DateAndYearSubtract.java
+++ myProgram/src/testPrograms/DateAndYearSubtract.java
@@ -3,26 +3,29 @@ package testPrograms;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 public class DateAndYearSubtract {
 
 	public static void main(String[] args) throws ParseException {
-		// TODO Auto-generated method stub
-		//System Date	
+
+//System Date	
 		SimpleDateFormat sydtformat = new SimpleDateFormat("yyyy-MM-dd");
 		Date SystemDate = new Date();
 		String sysdate = sydtformat.format(SystemDate);
 		System.out.println(sysdate);
+	
+//Subtraction of date		
+	/*Local Date only parse dateString in the format "yyyy-MM-dd" so use next line
+	 if the inputdate is in different format.
+		DateTimeFormatter dft = DateTimeFormatter.ofPattern("dd/MM/yyyy");*/
 		
-
-		
-		
-		//Subtraction of date
 		LocalDate date = LocalDate.parse(sysdate);
 		LocalDate returnvalue = date.minusDays(1);
-		System.out.println(returnvalue);
-		String returnvalueString = returnvalue.toString();
+		LocalDate returnvalue1 = returnvalue.minusYears(1);
+		System.out.println(returnvalue1);
+		String returnvalueString = returnvalue1.toString();
 		
 		SimpleDateFormat newformat = new SimpleDateFormat("yyyy-MM-dd");
 		Date myDate = newformat.parse(returnvalueString);
